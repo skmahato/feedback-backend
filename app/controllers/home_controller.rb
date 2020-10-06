225051a7642a4ed2de1b68c397c6d2c038182b3e
skmahato <souravkumar1329@gmail.com>
2020-10-06 15:30:20 +0530

    post review api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ class HomeController < ApplicationController
     def index
         if params[:token]
             id = params[:token]&.split('-')[0]
-            if id
-                reviews = Dealership.find_by(id: id)&.reviews&.only_visible
+            reviews = Dealership.find_by(id: id)&.reviews&.only_visible
+            if reviews
                 render json: reviews, include: { user: { only: :name }}
             else
                 render json: :ok
@@ -11,5 +11,39 @@ class HomeController < ApplicationController
         else
             render json: :ok
         end
+    end
+
+    def create
+        if params[:token]
+            id = params[:token]&.split('-')[0]
+
+            if params[:user]
+                user = User.new(user_params)
+                user.password = SecureRandom.urlsafe_base64
+
+                if user.save
+                    review = user.reviews.build(review_params)
+                    review.dealership_id = id
+
+                    if review.save
+                        render json: review, include: { user: { only: :name }}
+                    else
+                        render json: :unprocessable_entity, status: 404
+                    end
+                else
+                    render json: :unprocessable_entity, status: 404
+                end
+            end
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
+
+    def review_params
+        params.require(:review).permit(:title, :comment, :rating)
     end
 end
